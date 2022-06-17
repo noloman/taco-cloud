2efafc08d06e72e68efad438f7e072638e55ecc1
Manuel Lorenzo <manulorenzop@gmail.com>
2022-06-17 09:39:48 +0200

    add a WebConfig class that implements the WebMvcConfigurer so we can remove the useless HomeController

--- src/test/java/com/nulltwenty/tacocloud/HomeControllerTests.java
+++ src/test/java/com/nulltwenty/tacocloud/HomeControllerTests.java
@@ -1,16 +1,13 @@
 package com.nulltwenty.tacocloud;
 
-import com.nulltwenty.tacocloud.controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HomeController.class)
 public class HomeControllerTests {
     @Autowired
     private MockMvc mockMvc;
